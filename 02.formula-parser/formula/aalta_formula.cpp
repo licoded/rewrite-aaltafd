adb813d07871a24dbf071b19a910c5040dce6a0a
licoded <busy.li@foxmail.com>
2023-06-21 00:08:58 -0700

    parser: encounter a BUG
    no suitable conversion function from "aalta::aalta_formula" to "aalta::aalta_formula *"
    
    > when `right_ = aalta_formula(id)`

--- 02.formula-parser/formula/aalta_formula.cpp
+++ 02.formula-parser/formula/aalta_formula.cpp
@@ -7,6 +7,8 @@
 
 #include "aalta_formula.h"
 #include "ltlparser/trans.h"
+#include <unordered_map>
+#include <vector>
 
 namespace aalta {
     aalta_formula::aalta_formula() {} // new 时调用
@@ -16,6 +18,9 @@ namespace aalta {
         *this = orig; // 浅拷贝
     }
 
+    aalta_formula::aalta_formula(int atom_id)
+        : op_(atom_id)
+        {}
     aalta_formula::aalta_formula(OperatorType op,
                                  aalta_formula *left,
                                  aalta_formula *right)
@@ -29,9 +34,36 @@ namespace aalta {
     }
 
     aalta_formula::aalta_formula(const ltl_formula *formula, bool is_not)
+    // TODO: variable `is_not`, I want to change a more clear/articulate name
     {
         // build(formula, is_not);
     }
 
     aalta_formula::~aalta_formula() {}
+
+    /**
+     * 添加原子变量
+     * @param name
+     * @param is_not
+     */
+    inline void
+    aalta_formula::build_atom(const char *name, bool is_not)
+    {
+        int id;
+        const auto& it = ids.find(name);
+        if (it == ids.end())
+        { // 此变量名未出现过，添加之
+            id = names.size();
+            ids[name] = id;
+            names.push_back(name);
+        }
+        else
+        {
+            id = it->second;
+        }
+        if (is_not)
+            op_ = Not, right_ = aalta_formula(id);
+        else
+            op_ = id;
+    }
 } // namespace aalta_formula
--- 02.formula-parser/formula/aalta_formula.h
+++ 02.formula-parser/formula/aalta_formula.h
@@ -39,22 +39,26 @@ namespace aalta {
         ////////////
         //成员变量//
         //////////////////////////////////////////////////
-        OperatorType op_ = Undefined; // 操作符
+        int op_ = Undefined; // 操作符or操作数(原子atom)
         aalta_formula *left_ = nullptr; // 操作符左端公式
         aalta_formula *right_ = nullptr; // 操作符右端公式
         // int length_; //公式长度
         // aalta_formula *unique_ = nullptr; // 指向唯一指针标识
         // aalta_formula *simp_ = nullptr; // 指向化简后的公式指针
+        static std::vector<std::string> names; // 存储操作符的名称以及原子变量的名称
+        static std::unordered_map<std::string, int> ids; // 名称和对应的位置映射
         //////////////////////////////////////////////////
 
     public:
         aalta_formula(); // new 时调用
         aalta_formula(const aalta_formula& orig); // 拷贝构造函数
         aalta_formula(OperatorType op, aalta_formula *left, aalta_formula *right);
+        aalta_formula(int atom_id);
         aalta_formula(const char *input);
         aalta_formula(const ltl_formula *formula, bool is_not = false);
         ~aalta_formula();
-
+        void build (const ltl_formula *formula, bool is_not = false);
+        void build_atom(const char *name, bool is_not = false);
     };
 } // namespace aalta_formula
 
