111fd80f3aba4d84304a8f702e76735ce4d21753
licoded <busy.li@foxmail.com>
2023-06-26 16:19:00 +0800

    main: apply transfer

--- 02.formula-parser/main.cpp
+++ 02.formula-parser/main.cpp
@@ -1,7 +1,8 @@
 #include "formula/aalta_formula.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
 
 
 #define MAXN 100000
@@ -11,14 +12,22 @@ using namespace aalta;
 
 int main()
 {
+    aalta_formula::TAIL (); //set tail id to be 1
+
     puts ("please input the formula:");
     if (fgets (in, MAXN, stdin) == NULL)
     {
         printf ("Error: read input!\n");
         exit (0);
     }
+    aalta_formula* af = aalta_formula(in).unique();
+    std::cout << af->to_string () << std::endl;
 
-    aalta_formula* af;
+    // af = af->nnf();              // has been done in `build()` func
+    af = af->add_tail();
+    // af = af->remove_wnext();     // has been done in `build()` func
+    // af = af->simplify();         // just skip this now
+    af = af->split_next();
 
     return 0;
 }
