4ea0331394b9fcd345685253fe78ca7870ce7d0f
licoded <busy.li@foxmail.com>
2023-06-27 10:00:28 +0800

    solver: small opts for inline funcs

--- 02.formula-parser/solver.cpp
+++ 02.formula-parser/solver.cpp
@@ -13,7 +13,7 @@ namespace aalta
     ///////////inline functions
     /**
      * Check if `af *f` is in `clauses_added_`
-    */
+     */
     inline bool Solver::clauses_added(aalta_formula *f)
     {
         if (clauses_added_.find(f) != clauses_added_.end())
@@ -23,7 +23,7 @@ namespace aalta
 
     /**
      * Insert `af *f` into `clauses_added_`
-    */
+     */
     inline void Solver::mark_clauses_added(aalta_formula *f)
     {
         clauses_added_.insert(f);
@@ -31,33 +31,35 @@ namespace aalta
 
     /**
      * Insert `af *f` into `formula_map_`
-    */
+     */
     inline void Solver::build_formula_map(aalta_formula *f)
+    {
+        formula_map_.insert({get_SAT_id(f), f}); // {key, value}
+    }
+
+    /**
+     * Return the SAT_id of `af *f`
+     * NOTE: for !a, use -id (a) rather than id (!a);
+     *
+     * TODO: Why use `inline` instead of `static inline` to define this function?
+     *          - may because `static inline` cannot act as `inline` expectation
+     */
+    inline int Solver::get_SAT_id(aalta_formula *f)
     {
         /**
          * (OLD)COMMENTS: for !a, use `-id (a)` rather than `id (!a)`;
          * TODO: What is the difference of the two?
          *          - I just think the logic of `-id(a)` is more natural.
          *          - And the former may save a new id than the latter.
-        */
-        if (f->oper() == e_not)
-            formula_map_.insert(std::pair<int, aalta_formula *>(-f->r_af()->id(), f));
-        else
-            formula_map_.insert(std::pair<int, aalta_formula *>(f->id(), f));
-    }
-
-    /**
-     * Return the SAT_id of `af *f`
-     * NOTE: for !a, use -id (a) rather than id (!a);
-    */
-    inline int Solver::get_SAT_id(aalta_formula *f)
-    {
-        // for !a, use -id (a) rather than id (!a);
+         */
         if (f->oper() == e_not)
             return -f->r_af()->id();
         return f->id();
     }
 
+    /**
+     * Act as its name!
+    */
     inline void Solver::terminate_with_unsat()
     {
         unsat_forever_ = true;
