f8c34faf24e96733e5063a816b6a0b5a21b9ff44
licoded <busy.li@foxmail.com>
2023-06-28 13:50:04 +0800

    ltlfchecker: add comments for `dfs_check()` func

--- 02.formula-parser/ltlfchecker.cpp
+++ 02.formula-parser/ltlfchecker.cpp
@@ -16,7 +16,18 @@ namespace aalta
 
 	bool LTLfChecker::dfs_check(aalta_formula *f)
 	{
+		/**
+		 * NOTE: No matter which loop and which recursion, the SAT solver is the same one.
+		 * 		 It means that the `add_clause()` will accumulate all the time.
+		*/
 		visited_.push_back(f);
+		/**
+		 * NOTE: All cases will `pop_back(f)` after execution of current func, except `return true;`
+		 * 
+		 * TODO:
+		 * 		- why not just `return false;`?
+		 * 		- how can TRUE cases not `pop_back(f)`?
+		*/
 
 		if (detect_unsat())
 			return false;
@@ -24,6 +35,11 @@ namespace aalta
 			return true;
 		if (f->is_globally())
 		{
+			/**
+			 * My understanding:
+			 * 		As `sat_once(f)` is false, and f is G formula, so f cannot be SAT.
+			 * 		- TODO: sat_once() test, if current state is TAIL (ending state), whether f can be SAT.
+			*/
 			visited_.pop_back();
 			push_formula_to_explored(f);
 			return false;
@@ -45,16 +61,30 @@ namespace aalta
 		}
 		*/
 
-		// The SAT solver cannot return f as well
+		/**
+		 * OLD COMMENTS: The SAT solver cannot return f as well
+		 * 
+		 * TODO: Why?
+		 * 			- may in order to avoid dead loop? 
+		 * 				- So, it is bind with `visited_.pop_back()`
+		 * 					- No, it is about `add_cluase()`, not `visited_`
+		 * 				- But, it still in order to avoid dead loop, I think.
+		*/
 		push_formula_to_explored(f);
 
 		while (true)
 		{
 			if (detect_unsat())
 				return false;
+			/**
+			 * TODO: Why not check `sat_once()` here.
+			 * 			- I think it is because the below `get_one_transition_from(f)` has already do this.
+			 * 			- But the new problem is why `detect_unsat()` here?
+			 * 				- I think `detect_unsat()` is also included in `get_one_transition_from(f)`, so why?
+			*/
 
 			Transition *t = get_one_transition_from(f);
-			if (t != NULL)
+			if (t != NULL) // Tail /\ xnf(\fi) is SAT
 			{
 				if (dfs_check(t->next()))
 				{
@@ -62,7 +92,7 @@ namespace aalta
 					return true;
 				}
 			}
-			else // cannot get new states, that means f is not used anymore
+			else // UNSAT, cannot get new states, that means f is not used anymore
 			{
 				visited_.pop_back();
 				push_uc_to_explored();
@@ -72,11 +102,6 @@ namespace aalta
 		}
 		visited_.pop_back();
 		return false;
-		/**
-		 * TODO:
-		 * Why first `pop_back()` then `return false;`?
-		 * Why not just do `return false;` without `pop_back()`
-		 */
 	}
 
 	void LTLfChecker::push_formula_to_explored(aalta_formula *f)
