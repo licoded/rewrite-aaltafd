76d573510b0c195281aab19a244912261e746b76
licoded <busy.li@foxmail.com>
2023-07-02 10:47:36 +0800

    main: add and change to CARChecker

--- 02.formula-parser/main.cpp
+++ 02.formula-parser/main.cpp
@@ -1,10 +1,10 @@
 #include "formula/aalta_formula.h"
 #include "ltlfchecker.h"
+#include "carchecker.h"
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
-
 
 #define MAXN 100000
 char in[MAXN];
@@ -13,16 +13,16 @@ using namespace aalta;
 
 int main()
 {
-    aalta_formula::TAIL (); //set tail id to be 1
+    aalta_formula::TAIL(); // set tail id to be 1
 
-    puts ("please input the formula:");
-    if (fgets (in, MAXN, stdin) == NULL)
+    puts("please input the formula:");
+    if (fgets(in, MAXN, stdin) == NULL)
     {
-        printf ("Error: read input!\n");
-        exit (0);
+        printf("Error: read input!\n");
+        exit(0);
     }
-    aalta_formula* af = aalta_formula(in).unique();
-    std::cout << af->to_string () << std::endl;
+    aalta_formula *af = aalta_formula(in).unique();
+    std::cout << af->to_string() << std::endl;
 
     // af = af->nnf();              // has been done in `build()` func
     // af = af->remove_wnext();     // has been done in `build()` func
@@ -31,10 +31,21 @@ int main()
     // af = af->simplify();         // just skip this now
 
     std::cout << "=== after all transfer" << std::endl;
-    std::cout << af->to_string () << std::endl;
+    std::cout << af->to_string() << std::endl;
 
-    LTLfChecker checker (af);
-    bool res = checker.check();
+    const bool BLSC = false;
+
+    bool res;
+    if (BLSC)
+    {
+        LTLfChecker checker(af);
+        res = checker.check();
+    }
+    else
+    {
+        CARChecker checker(af);
+        res = checker.check();
+    }
     printf("%s\n", res ? "sat" : "unsat");
 
     return 0;
